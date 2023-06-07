d3e8223317ed0955f7e6f10296de9c5a3e0f61b8
RevadiSundaram <sundaramrevadi@gmail.com>
2023-06-07 20:58:52 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const API_URL = "https://randomuser.me/api";
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const json = await axios.get(API_URL);
+
+      const userName = json.data.results[0].name;
+      const userFullName =
+        userName.title + " " + userName.first + " " + userName.last;
+      setName(userFullName);
+      localStorage.setItem("name", JSON.stringify(userFullName));
+
+      const userEmail = json.data.results[0].email;
+      setEmail(userEmail);
+      localStorage.setItem("email", JSON.stringify(userEmail));
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <h1>Application Page</h1>
+        <h3>Full Name:</h3>
+        <p>{name}</p>
+        <h3>Email: </h3>
+        <p>{email}</p>
+        <button type="button" onClick={fetchData}>
+          Refresh
+        </button>
+      </form>
     </div>
   );
 }
-
-export default App;
